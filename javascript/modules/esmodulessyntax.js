3165729016d9253f89ac118742a5ffe3ed46acf8
Yuri Melo <yuri.ifal@gmail.com>
2018-03-03 22:43:16 -0300

    modules JS

---
+++ javascript/modules/esmodulessyntax.js
@@ -0,0 +1,11 @@
+// function foo () {
+// 	export default 'bar'
+// }
+// foo()
+
+
+
+function foo() {
+	return 'bar'
+}
+export default foo
---
+++ javascript/modules/modules1.js
@@ -0,0 +1,7 @@
+module.exports = 1
+module.exports = NaN
+module.exports = 'foo'
+module.exports = { foo: 'bar' }
+module.exports = [ 'foo', 'bar' ]
+module.exports = function foo () {}
+module.exports = () => {}
