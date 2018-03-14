436d02bceeb615ac98bce2cbae7691f6b76d5714
Yuri Melo <yuri.ifal@gmail.com>
2018-03-13 21:51:01 -0300

    a little comment

--- rails/recipes/app/controllers/recipe_controller.rb
+++ rails/recipes/app/controllers/recipe_controller.rb
@@ -3,6 +3,7 @@ class RecipeController < ApplicationController
   	@recipes = Recipe.all
   end
 
+  # O params gera um hash com todas as informações
   def show
   	@recipe = Recipe.find(params[:id])
   end
