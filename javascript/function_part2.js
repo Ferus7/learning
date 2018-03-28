4639921faa056626af2cb2b8777737a43a237c62
yuridsm <yuri.ifal@gmail.com>
2018-03-28 06:56:07 -0300

    reviewing

--- javascript/function_part2.js
+++ javascript/function_part2.js
@@ -16,33 +16,46 @@
 // console.log(add('A'));
 // console.log(add('B'));
 // console.log(add('C'));
-
 // console.log(add());
 
 /*
+ * JavaScript não tem linker, como em outras linguagens, por causa disso temos 
+ * que ter cuidado com o escopo global.
+ *
+ * O nosso problema começa quando a variável é declarada em escopo global.
+ *
+ * Duas funções com o mesmo nome estão se referenciando a ela, nas suas chamadas
+ * (linhas 16-19); o problema é quando declara-se outra função com o mesmo nome,
+ * pois a partir deste momento, toda vez que você se referenciar a uma delas, o
+ * interpretador tomará como referencia a última função declarada.
+ *
+ * Por causa disso, na linha 19 ocorrerá algo inesperado, pois ele vai tentar
+ * colocar o nada, pois não há parâmetro, em itens, portanto, será adicionado undefined.
+ *
  * Uma das formas para resolvemos este tipo de problema
- * é com o uso emcapsulamento por meio de objetos;
+ * é com o uso do emcapsulamento por meio de objetos;
  * No entanto, este tipo de solução gera um problema, pois
  * Javascript não tem modificadores de visibilidade (public, private),
  * portanto, corre um sério risco de acessármos um valor
  * interno ao objeto.
 */
 
-// var counter = {
-// 	value: 0,
-// 	add: function() {
-// 		return ++this.value;
-// 	}
-// };
+var counter = {
+	value: 0,
+	add: function() {
+		return ++this.value;
+	}
+};
 
-// console.log(counter.add());
-// console.log(counter.add());
-// console.log(counter.add());
+console.log(counter.add());
+console.log(counter.add());
+// counter.value = undefined;
+console.log(counter.add());
 
 /* Temos aqui um NaN, pois ele está somando ++undefined, e isso
  * não existe. Então temos um sério problema de podermos 
  * modificar uma propriedade dentro de um objeto.
- * 			counter.value = undefined
+ * 			counter.value = undefined;
  */
 
 // counter.value = undefined;
@@ -64,18 +77,18 @@
  */
 
 /* por meio de Factory Function */
-var createCounter = function() {
-	var value = 0;
-	return {
-		add: function() {
-			// Isso só é possível por meio das closures
-			return ++value;
-		}
-	};
-};
+// var createCounter = function() {
+// 	var value = 0;
+// 	return {
+// 		add: function() {
+// 			// Isso só é possível por meio das closures
+// 			return ++value;
+// 		}
+// 	};
+// };
 
-var counter = createCounter();
-console.log(counter.add());
+// var counter = createCounter();
+// console.log(counter.add());
 
 /* Por meio de Constructor Function */
 // var Create = function() {
