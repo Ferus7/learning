f7a4453dddede39bb835e62035fc1a8fcdf1e891
yuridsm <yuri.ifal@gmail.com>
2018-03-30 10:14:33 -0300

    js

--- javascript/functions/closures.js
+++ javascript/functions/closures.js
@@ -1,22 +1,21 @@
-var helloWorld = function() {
-	var message = "Hello World!";
-	return function() {
-		return message;
-	};
-};
+// var helloWorld = function() {
+// 	var message = "Hello World!";
+// 	return function() {
+// 		return message;
+// 	};
+// };
 
 // var fnHelloWorld = helloWorld();
 // console.log(fnHelloWorld());
 
-function makeFunc() {
-	var name = "Mozilla";
-	function displayName() {
-		return name;
-	}
-	return displayName;
-}
+// function makeFunc() {
+// 	var name = "Mozilla";
+// 	function displayName() {
+// 		return name;
+// 	}
+// 	return displayName;
+// }
 
-var myFunc = makeFunc();
-
+// var myFunc = makeFunc();
 
 
