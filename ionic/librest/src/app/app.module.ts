c4e64a20fdf723e5b0e5db5df282a778624a6d62
yuridsm <yuri.ifal@gmail.com>
2018-04-02 21:37:41 -0300

    module TS

--- ionic/librest/src/app/app.module.ts
+++ ionic/librest/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MyBooksPage } from '../pages/myBooks/myBooks';
 
 import { DetailsPageModule } from '../pages/details/details.module';
 import { ReviewPageModule } from '../pages/review/review.module';
 import { ReadingPageModule } from '../pages/reading/reading.module';
-import { MenuPageModule } from '../pages/menu/menu.module';
+import { AllPageModule } from '../pages/all/all.module';
+import { ReadPageModule } from '../pages/read/read.module';
+import { NonReadPageModule } from '../pages/non-read/non-read.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,10 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    MyBooksPage
   ],
   imports: [
     BrowserModule,
@@ -30,15 +28,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     DetailsPageModule,
     ReviewPageModule,
     ReadingPageModule,
-    MenuPageModule
+    AllPageModule,
+    ReadPageModule,
+    NonReadPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    MyBooksPage
   ],
   providers: [
     StatusBar,
