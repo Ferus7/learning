6845547250c71d389f5996b04cd6a12d265e72df
yuridsm <yuri.ifal@gmail.com>
2018-04-03 14:50:31 -0300

    files

--- ionic/librest/src/app/app.component.ts
+++ ionic/librest/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
-//import { MyBooksPage } from '../pages/myBooks/myBooks';
+import { MyBooksPage } from '../pages/myBooks/myBooks';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  //rootPage:any = MyBooksPage;
+  // rootPage:any = MyBooksPage;
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
