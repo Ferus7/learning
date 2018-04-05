c92b28c707f119c3aa9d0e11d53c82ff88f0a7e2
yuridsm <yuri.ifal@gmail.com>
2018-04-05 08:21:40 -0300

    files

--- angular/primeiro-projeto/src/app/app.module.ts
+++ angular/primeiro-projeto/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    MeuSegundoComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/primeiro-projeto/src/app/meu-segundo/meu-segundo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'yr-tag',
+	template: `
+		<h1>Welcome To Program</h2>
+		<h3>Yuri Melo</h3>
+		<h6>Apr 5, 2018</h6>
+	`
+})
+
+export class MeuSegundoComponent { }
